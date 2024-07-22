c5386ffca65660c2bf225ebe4c6d132debfdf8b4
Mohsin Pannu <mohsin.pannu170@gamil.com>
2024-07-22 22:13:14 +0500

    added functioning backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
-import products from '../products'
 import { Link } from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
+import axios from 'axios'
 
 const ProductScreen = () => {
+    const [product, setProduct] = useState({})
     const { id: productId } = useParams()
-    const product = products.find((p) => p._id === productId)
-    console.log(product)
+    
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data} = await axios.get(`/api/products/${productId}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [])
 
     return (
         <>
