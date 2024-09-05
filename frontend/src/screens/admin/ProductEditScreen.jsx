429ce2591ff8947f01000890132176f53e858c64
Mohsin Pannu <mohsin.pannu170@gamil.com>
2024-09-05 15:52:47 +0500

    Image upload

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -5,7 +5,7 @@ import Message from "../../components/Message"
 import Loader from "../../components/Loader"
 import FormContainer from "../../components/FormContainer"
 import { toast } from "react-toastify"
-import { useUpdateProductMutation, useGetProductDetailsQuery } from "../../slices/productsApiSlice"
+import { useUpdateProductMutation, useGetProductDetailsQuery, useUploadProductImageMutation } from "../../slices/productsApiSlice"
 
 const ProductEditScreen = () => {
     const {id: productId} = useParams()
@@ -22,6 +22,8 @@ const ProductEditScreen = () => {
     console.log(product)
 
     const [updateProduct, {isLoading: loadingUpdate}] = useUpdateProductMutation()
+
+    const [uploadProductImage, {isLoading: loadingUpload}] = useUploadProductImageMutation()
 
     const navigate = useNavigate()
 
@@ -58,6 +60,17 @@ const ProductEditScreen = () => {
         }
     }
  
+  const uploadFileHandler = async (e) => {
+    const formData = new FormData();
+    formData.append('image', e.target.files[0]);
+    try {
+      const res = await uploadProductImage(formData).unwrap();
+      toast.success(res.message);
+      setImage(res.image);
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
   return (
     <>
         <Link to='/admin/productlist' className="btn btn-light ny-3">Go back</Link>
@@ -88,7 +101,21 @@ const ProductEditScreen = () => {
                         ></Form.Control>
                     </Form.Group>
 
-                    {/* Image input placeholder */}
+                    <Form.Group controlId='image'>
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter image url'
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              ></Form.Control>
+              <Form.Control
+                label='Choose File'
+                onChange={uploadFileHandler}
+                type='file'
+              ></Form.Control>
+              {loadingUpload && <Loader />}
+            </Form.Group>
 
                     <Form.Group controlId="brand">
                         <Form.Label>Brand</Form.Label>
